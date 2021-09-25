80e1aaeb8140e0fa401f6e2ad8ee1d2872e1eba1
HEYSEL ARNEDO <heysel82@gmail.com>
2021-09-25 13:14:17 -0500

    Update Equipo.vue

--- Parking_VUE/src/views/Equipo.vue
+++ Parking_VUE/src/views/Equipo.vue
@@ -15,7 +15,18 @@
         Lo que debes hacer es registrarte y usar nuestro directorio
         <br>
         El paqueadero es un sistema que posee una plataforma Movil. Dicho dispositivo puede agendar su estacionamiento de los vehiculos y asi evitar las filas para hacer el ingreso a la plataforma. </p>
+    </div><<br>
+    <div class="hero is-danger">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">¿Que Ofrecemos?</h1>
+        </div>
+      </div>
     </div>
+    <div class="container">
+      <p class="org-description is-size-4">
+        Ademas de ofrecer servicio de parqueo, tambien ofrecemos servicio de lavanderia para automoviles, ofrecemos una plataforma para servicio automatizado de parqueo Online.</p>
+    </div><br><br><br>    
   </div>
 </template>
 <style lang="scss" scoped>
