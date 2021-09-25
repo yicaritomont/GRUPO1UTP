a00f3164eb50c6013b3e2fdb2f888e4afc1cb663
HEYSEL ARNEDO <heysel82@gmail.com>
2021-09-25 13:21:52 -0500

    Update Equipo.vue

--- Parking_VUE/src/views/Equipo.vue
+++ Parking_VUE/src/views/Equipo.vue
@@ -26,7 +26,18 @@
     <div class="container">
       <p class="org-description is-size-4">
         Ademas de ofrecer servicio de parqueo, tambien ofrecemos servicio de lavanderia para automoviles, ofrecemos una plataforma para servicio automatizado de parqueo Online.</p>
-    </div><br><br><br>    
+    </div><br>
+    <div class="hero is-danger">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">¿Donde estamos Ubicados?</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="org-description is-size-4">
+        Estamos Ubicados en Bogota.</p>
+    </div><br><br><br> 
   </div>
 </template>
 <style lang="scss" scoped>
